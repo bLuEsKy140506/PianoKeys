ef14ca2a0c0f6b997b6b7395d165d3a5e4b7b318
bLuEsKy140506 <115715458+bLuEsKy140506@users.noreply.github.com>
2025-03-21 23:13:00 +0800

    instruction code until step...12

--- main.js
+++ main.js
@@ -55,8 +55,19 @@ nextThree.hidden = true;
 startOver.hidden = true;
 
 // Write anonymous event handler property and function for the first progress button
+nextOne.onclick = function () {
+  nextTwo.hidden = false;
+  nextOne.hidden = true;
+
+  document.getElementById("letter-note-five").innerHTML = "D";
+  document.getElementById("letter-note-six").innerHTML = "C";
+};
 
 // Write anonymous event handler property and function for the second progress button
+nextTwo.onclick = function () {
+  nextTwo.hidden = true;
+  nextThree.hidden = false;
+};
 
 // Write anonymous event handler property and function for the third progress button
 
